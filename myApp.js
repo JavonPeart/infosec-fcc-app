ae928b0bf4d6ceadf7f22eb4e93de3e253514a0d
JavonPeart <44687372+JavonPeart@users.noreply.github.com>
2023-09-03 23:50:07 -0500

    Task 7

--- myApp.js
+++ myApp.js
@@ -5,10 +5,14 @@ const bcrypt = require('bcrypt');
 
 
 
-app.use(helmet.ieNoOpen());
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
-
-
+app.use(
+  helmet.hsts({
+    maxAge: ninetyDaysInSeconds,
+    force: true,
+  })
+);
 
 
 
